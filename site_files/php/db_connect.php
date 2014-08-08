36e48b67080b810a6e28eb3e1d8306d322cce0ca
Jason Malutich <malutj@users.noreply.github.com>
2014-08-08 14:13:07 -0500

    Update db_connect.php

--- site_files/php/db_connect.php
+++ site_files/php/db_connect.php
@@ -1,5 +1,5 @@
 <?php
-	$dbhost = ''; //url for host
+    $dbhost = ''; //url for host
 	$dbname = ''; //name of the database
 	$dbuser = ''; //username for the database login
 	$dbpass = ''; //password for the database login
