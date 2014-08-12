fc223f1c478a6cb3d2b008cc65553d57db239f6f
Jason Malutich <malutj@users.noreply.github.com>
2014-08-12 14:20:10 -0500

    Update admin.js

--- site_files/js/admin.js
+++ site_files/js/admin.js
@@ -27,63 +27,17 @@ $(document).ready(function(){
   $("#add_button").click(function(){
     $.post("./php/clients.php",
       {requestType : 'add',client_name : $("#client_name").val()},
-      function(data){
-        if(data.status==="success"){
-          populate_todo_list();
-        }
-        else if(data.status==="fail"){
-          //handle failure message
-        }
-        else{
-          //not sure what message we got back
-        }
-
-      }, "json");
-  });
-
-  //handles the delete button
-  $("#todo_list").on("click", ".control_panel", function(){
-    var todo_id = $(this).parent().attr('id');
-    $.post("./php/data.php",
-      {requestType : 'delete', todo_id : todo_id},
       function(result){
         if(result.status==="success"){
-          populate_todo_list();
+          //add client to global array and reprint todo list
         }
         else{
-          $("#todo_list").html("There seems to have been an error");
+          //handle failure message
         }
+
       }, "json");
   });
-
- //----------USER-DEFINED FUNCTIONS----------//
-
-  function populate_todo_list(){
-    $.post("./php/data.php", 
-      {requestType : 'load'}, 
-        function(result){
-          if(result.status==="success"){
-            if(result.data.length > 0){
-              var new_html="<ul>";
-                $.each(result.data, function(key, entry){
-                  var next_todo = "<li><div class='todo' id ='"+
-                      entry.todo_id+"'><div class='control_panel'>DELETE</div><div class='todo_content'>"+
-                      entry.todo_text+"</div></div></li>";
-
-                  new_html = new_html + next_todo;
-                });
-                new_html= new_html +"</ul>";
-                $("#todo_list").html(new_html);
-                $("#todo_list").append("<div style='text-align:left; margin-left:45px; margin-bottom: -15px; color: #1BC9F2;'>Click on a to-do item to delete</div>");
-            }
-            //no to-dos were found
-            else{
-              $("#todo_list").html("");
-            }
-          }
-          else{
-            $("#todo_list").html("Sorry, we were unable to load your to-do list");
-          }
-        },'json');
-  }
 });
+
+
+
