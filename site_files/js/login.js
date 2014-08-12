776c16c7715f5a5f0f26e295282d0a2777693dab
Jason Malutich <malutj@users.noreply.github.com>
2014-08-12 09:42:32 -0500

    Update login.js

--- site_files/js/login.js
+++ site_files/js/login.js
@@ -6,6 +6,43 @@
 
 $(document).ready(function(){
 
+//----------ANIMATIONS----------//
+
+    //Username text field
+	$("#username")
+      .focus(function(){
+	    if($("#username").val()===" USERNAME"){
+          $("#username")
+            .val("")
+            .css("color","black"); 
+	    }
+	  })
+      .focusout(function(){
+		if($("#username").val()===""){
+	    	$("#username")
+              .val(" USERNAME")
+              .css("color","gray");
+	    }
+	});
+
+    //Password text field
+	$("#password")
+      .focus(function(){
+	    if($("#password").val()===" PASSWORD"){
+	    	$("#password")
+              .val("")
+	    	  .prop("type","password")
+              .css("color","black");
+	    }
+	  })
+      .focusout(function(){
+		if($("#password").val()===""){
+	    	$("#password")
+              .val(" PASSWORD")
+	    	  .prop("type","text")
+              .css("color","gray");
+	    }
+	});
 	//Handles login button press
 	$("#login_button").click(function(){
 		if(validate()){
