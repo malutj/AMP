596e3a4078191e5ed872ac59dee3fcaa0894258a
Jason M Malutich <jmalutich@gmail.com>
2014-08-14 00:52:00 +0000

    update database connection info

--- site_files/php/db_connect.php
+++ site_files/php/db_connect.php
@@ -1,6 +1,6 @@
 <?php
-    $dbhost = ''; //url for host
-    $dbname = ''; //name of the database
-    $dbuser = ''; //username for the database login
-    $dbpass = ''; //password for the database login
+    $dbhost = 'http://necron99-123530.usw1.nitrousbox.com/'; //url for host
+    $dbname = 'miller'; //name of the database
+    $dbuser = 'malutj'; //username for the database login
+    $dbpass = 'j@ys0n!'; //password for the database login
 ?>
