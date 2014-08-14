747118db4c9e14db81626f7217aa3c79b2d933cc
Jason Malutich <malutj@users.noreply.github.com>
2014-08-14 10:58:06 -0500

    Update sync.php

--- site_files/php/sync.php
+++ site_files/php/sync.php
@@ -1,9 +1,24 @@
-<php
+<?php
 //Turn on error reporting
 ini_set('display_errors', 'On');
 
 //include the database connection info
 include('database_connect.php');
 
+//verify that request came from app
+
+//create file list array
+
+//fetch list of all common files and save paths and mod dates
+
+//create PDO object
+
+//get directory location for this client
+
+//fetch list of all client-specific files save paths and mod dates
+
+//overwrite common files with client-specific files where required
+
+//return client file list
 
 ?>
