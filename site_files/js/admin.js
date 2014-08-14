00d06870a5c671df9fb78d6cc393c3982c2d4bf9
Jason Malutich <malutj@users.noreply.github.com>
2014-08-14 09:40:19 -0500

    Update admin.js

--- site_files/js/admin.js
+++ site_files/js/admin.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 //----------ACTIONS----------//
   //Checks whether the user is logged in or not on load
   $.post("./php/validate.php",
-    {requestType : 'validate'},
+    {request_type : 'validate'},
     function(result){
       if(result.status==="not logged in"){
         window.location = "login.html";
@@ -16,25 +16,27 @@ $(document).ready(function(){
   //handles the logout button
   $("#logout_button").click(function(){
     $.post("./php/validate.php", 
-          {requestType : 'logout'}, 
+          {request_type : 'logout'}, 
           function(result){
             window.location = "login.html";
     });
   });
 
   //handles the add button
-  $("#add_button").click(function(){
-    $.post("./php/clients.php",
-      {requestType : 'add',client_name : $("#client_name").val()},
-      function(result){
-        if(result.status==="success"){
-          //add client to global array and reprint todo list
-        }
-        else{
-          //handle failure message
-        }
+    $("#add_button").click(function(){
+        $.post("./php/clients.php",
+          {request_type : 'add',
+           client_name : $("#client_name").val()
+          },
+          function(result){
+            if(result.status==="success"){
+              //add client to global array and reprint todo list
+            }
+            else{
+              //handle failure message
+            }
 
-      }, "json");
+        }, "json");
   });
 });
 
